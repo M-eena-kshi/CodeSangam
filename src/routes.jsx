3c3077fa507d97593f6da10dd93b2125a4234082
khushi verma <akakhushiverma@gmail.com>
2025-11-15 11:58:01 +0530

    Initial commit

---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+// routes.jsx
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // You can define children routes here too
+  }
+]);
